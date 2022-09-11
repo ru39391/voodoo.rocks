58cf1575db9513595b0b0b22cbf613c8060643fa
ru39391 <dev@39391.ru>
2022-09-12 02:40:37 +0300

    Updates

---
+++ precache-manifest.88434caa82fd023703163635913f7703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9384a859b51c3aa9f9453879936678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f92a26df081381db45",
+    "url": "/static/css/main.c10e66c7.chunk.css"
+  },
+  {
+    "revision": "9294b00038db20a7cef0",
+    "url": "/static/js/2.d372e93a.chunk.js"
+  },
+  {
+    "revision": "e5f92a26df081381db45",
+    "url": "/static/js/main.56a8daec.chunk.js"
+  },
+  {
+    "revision": "6566005673492017d5f7",
+    "url": "/static/js/runtime-main.9d9d1fa3.js"
+  }
+]);
